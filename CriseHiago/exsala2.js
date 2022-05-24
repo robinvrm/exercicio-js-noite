347e8bf1bd7d95c584df445c42073dd406b0881a
Noemi Naiara Paes <noeminaiarap@gmail.com>
2022-05-24 18:03:05 -0300

    exercicio

---
+++ CriseHiago/exsala2.js
@@ -0,0 +1,36 @@
+var x=2;
+var y=4;
+var operacao = "adicao";
+var z;
+
+switch (operacao)
+{
+case "adicao":
+z= x+y;
+console.log ("os valores escolhidos são " + x +" e " + y +
+" e foi feita uma adição com resultado de " + z);
+break;
+
+case "subtracao":
+z= x-y;
+console.log ("os valores escolhidos são " + x +" e " + y +
+" e foi feita uma subtração com resultado de " + z);
+break;
+
+case "multiplicacao":
+z= x*y;
+console.log ("os valores escolhidos são " + x + " e " + y +
+" e foi feita uma multiplicaçao com resultado de " + z);
+break;
+
+case "divisao":
+z= x/y;
+console.log ("os valores escolhidos são " + x +" e " + y +
+" e foi feita uma divisao com resultado de " + z);
+break;
+
+default:
+
+
+
+}
