a05c05e6a7612616e60ebb49591b6cad35f0ef52
email <mateusturek1997@gmail.com>
2022-05-24 19:50:26 -0300

    matheus.js

---
+++ Matheus/javascriptfuncoes.js
@@ -0,0 +1,58 @@
+// // let verde = 10
+// let amarelo = 12
+// let vermelho = 15
+// let azul = 25
+
+// let cor = "verde"
+// if (cor == "verde"){
+//     console.log("não divisivel "+ verde);
+// }
+
+// let cor2 = "amarelo"
+// if (cor2 == "amarelo"){
+//     console.log("divisivel " + amarelo);
+// }
+
+// let cor3 = "vermelho"
+// if (cor3 == "vermelho"){
+//     console.log("divisivel " + vermelho);
+// }
+
+// let cor4 = "azul"
+// if (cor4 == "azul"){
+//     console.log("não divisivel " + azul);
+// }
+
+
+
+
+let result = 0
+
+for (let i= 100; i <= 200; i++){
+    result = i % 2;
+
+if (result != 0){
+    console.log(i);
+}
+
+
+}
+
+
+
+ 
+
+
+
+
+
+// let resultt = 0
+
+// for (let i= 1; i <= 500; i++){
+//     resultt = i % 2;
+
+//     if (resultt != 0) {
+//         console.log(i);
+//     }
+// }
+
