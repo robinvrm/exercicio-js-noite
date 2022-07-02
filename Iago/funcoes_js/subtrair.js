7fafc2695a0d5551e8bac1ebf990bb0e6a513c9e
Iago Goncalves <iago.g.goncalves@gmail.com>
2022-07-02 12:27:15 -0300

    calculadora imc

--- Iago/funcoes_js/subtrair.js
+++ Iago/funcoes_js/subtrair.js
@@ -6,11 +6,11 @@ function subtrair(num1, num2) {
 
 }
 
-let n1 = 20;
-let n2 = 5;
+// let n1 = 20;
+// let n2 = 5;
 
-var retornofuncao = subtrair(n1, n2)
-console.log(retornofuncao);
+// var retornofuncao = subtrair(n1, n2)
+// console.log(retornofuncao);
 
 function calcular(opc) {
 
